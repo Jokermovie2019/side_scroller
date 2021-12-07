76a8321a562b1457ebe0a573a70d0669e87bf944
Jokermovie2019 <sjpitman22@students.bmchs.org>
2021-12-07 08:28:46 -0600

    finished

--- side_scroller.py
+++ side_scroller.py
@@ -28,7 +28,7 @@ font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
  
-background = pygame.image.load("AnimatedStreet.png")
+background = pygame.image.load("cloudsky.jpg")
  
 #create a white screen
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -39,7 +39,7 @@ pygame.display.set_caption("Game")
 class Booster(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("boost.png")
+        self.image = pygame.image.load("boosto.png")
         self.rect = self.image.get_rect()
         self.rect.center = (1000, random.randint(40, SCREEN_HEIGHT - 40))
         self.wait = 0
@@ -66,25 +66,51 @@ class Robot(pygame.sprite.Sprite):
         pass
 
     
+class Lilbot(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("tomat2.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (1000, random.randint(40, SCREEN_HEIGHT - 40))
+        self.wait = 40
+        self.speed = -7
+
+    def move(self):
+        self.rect.move_ip(self.speed, 0)
+        if (self.rect.left < 0):
+            self.wait += 20
+            self.rect.left = 1000
+            self.rect.center = (1000, random.randint(40, SCREEN_HEIGHT - 40))
+            self.speed = (random.randint(-12, -5))
+    
+        
+        
+
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
-        self.image = pygame.image.load("pig.png")
+        self.image = pygame.image.load("superpig.png")
         self.rect = self.image.get_rect()
         self.rect.center = (400, 520)
         self.fall_speed = 1
         self.boost = 0
+    def speed_up(self, speedy):
+        if speedy > 6 and speedy <= 20:
+            self. fall_speed = 2
+        if speedy>12:
+            self.fall_speed = 3
+
 
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-        if self.rect.top < SCREEN_HEIGHT:
+        if self.rect.top < (SCREEN_HEIGHT-5):
             if pressed_keys[K_UP]:
-                self.rect.move_ip(0, -5)
-        if self.rect.top > 0:
+                self.rect.move_ip(0, -7)
+        if self.rect.bottom > 5:
             if pressed_keys[K_DOWN]:
-                self.rect.move_ip(0,5)
+                self.rect.move_ip(0,7)
          
         if self.boost > 0:
             if self.rect.right < (SCREEN_WIDTH-75):        
@@ -92,42 +118,58 @@ class Player(pygame.sprite.Sprite):
                     self.rect.move_ip(10, 0)
                     self.boost -= 1
 
+        if self.rect.left > (0):        
+                if pressed_keys[K_LEFT]:
+                    self.rect.move_ip(-5, 0)
+
         self.rect.move_ip(-self.fall_speed, 0)
 
 #setting up sprites
 P1 = Player()
 E1 = Booster()
 R1 = Robot()
+B1 = Lilbot()
+B2 = Lilbot()
 
 #creating sprites group
 enemies = pygame.sprite.Group()
 enemies.add(R1)
+enemies.add(B1)
+enemies.add(B2)
 boosters = pygame.sprite.Group()
 boosters.add(E1)
+
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(R1)
+all_sprites.add(B1)
+all_sprites.add(B2)
+
  
 #adding a new user event
 INC_SPEED = pygame.USEREVENT + 1 
 pygame.time.set_timer(INC_SPEED, 1000)
 
+#variable to help increase speed slowly
+speedy = 0
+
+
 #game loop
 while True:     
     #Cycles through all events occuring  
     for event in pygame.event.get():
-        if event.type == INC_SPEED:
-              SPEED += 0.5
-           
+        
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
     DISPLAYSURF.fill(WHITE)
     DISPLAYSURF.blit(background, (0,0))
-    scores = font_small.render(str(SCORE), True, BLACK)
-    DISPLAYSURF.blit(scores, (10,10))
+    
 
+    P1.speed_up(speedy)
+
+  
 
     #Moves and Re-draws all Sprites
     for entity in all_sprites:
@@ -143,11 +185,18 @@ while True:
 
 
 
-    #To be run if collision occurs between Player and Enemy
+    #To be run if collision occurs between Player and BOOST
+    boost_score = P1.boost
 
     if pygame.sprite.spritecollideany(P1, boosters):
-        P1.boost += 40
-        E1.wait += 10
+        P1.boost += 25
+        E1.wait += 20
+        speedy += 1
+        
+
+    scores = font_small.render(str(boost_score), True, BLACK)
+    DISPLAYSURF.blit(scores, (500,10))
+        
         
     if pygame.sprite.spritecollideany(P1, enemies):
         pygame.mixer.Sound('crash.wav').play()
